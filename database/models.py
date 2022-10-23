e3f4981396298b23380edd69587d88e464a686a1
lunarbunny <tingwei1734@gmail.com>
2022-10-23 14:54:05 +0800

    Update database

--- database/models.py
+++ database/models.py
@@ -4,12 +4,13 @@ from flask_login import UserMixin
 
 # Inheriting from UserMixin required for flask login
 class User(UserMixin):
-    def __init__(self, id, username, password, name, branchId):
+    def __init__(self, id, username, password, name, branchId, roleId):
         self.id = id
         self.username = username
         self.password = password
         self.name = name
         self.branchId = branchId
+        self.roleId = roleId
 
     @staticmethod
     def fromTuple(data):
@@ -33,16 +34,16 @@ class Donor:
 
 
 class BloodDonation:
-    def __init__(self, donationId: int, donorNRIC: str, quantity: float, date: datetime, time, branchId):
+    def __init__(self, donationId: int, donorNRIC: str, quantity: float, date: datetime, branchId: int, recordedBy: int):
         self.id = donationId
         self.nric = donorNRIC
         self.quantity = quantity
         self.date = date
-        self.time = time
         self.branchId = branchId
+        self.recordedBy = recordedBy
 
     def toTuple(self):
-        return (self.id, self.nric, self.quantity, self.date, self.time, self.branchId)
+        return (self.id, self.nric, self.quantity, self.date, self.branchId, self.recordedBy)
 
     @staticmethod
     def fromTuple(data):
